4d67ee6f95178baacd81d6c37486515b91b33da9
Felipe Teixeira <felipeantonioteixeira@gmail.com>
2020-08-26 16:55:02 -0300

    node.js challenge finished

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,16 +1,41 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+
+interface Input {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+};
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
   constructor(transactionsRepository: TransactionsRepository) {
     this.transactionsRepository = transactionsRepository;
-  }
+  };
 
-  public execute(): Transaction {
-    // TODO
-  }
-}
+  public execute({ title, value, type }: Input): Transaction {
+
+    if (['income', 'outcome'].includes(type)) {
+      const { total } = this.transactionsRepository.getBalance();
+
+      if (type === 'outcome' && total < value) {
+        throw new Error('You do not have enough balance');
+      }
+
+      const transaction = this.transactionsRepository.create({
+        title,
+        value,
+        type
+      });
+
+      return transaction;
+
+    } else {
+      throw new Error('Plase type a valid type!');
+    }
+  };
+};
 
 export default CreateTransactionService;
